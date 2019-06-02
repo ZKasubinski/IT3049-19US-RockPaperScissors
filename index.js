dc2127b921ceb2031a17f8c3d3dbc5c8df5493bf
kasubiaj <kasubiaj@mail.uc.edu>
2019-06-02 17:58:56 -0500

    Finished second form and added logic to the game. Need to display who wins next

--- index.js
+++ index.js
@@ -7,29 +7,90 @@ var name;
 const homeScreen = document.getElementById("homeScreen");
 const gameScreen = document.getElementById("gameScreen");
 
+const enterChoiceFrm = document.getElementById("enterChoiceFrm");
+var decision = document.getElementById("decision");
+
 let game;
 
 class RockPaperScissors
 {
 	constructor()
 	{
-		
+		this.randomNumber = Math.floor((Math.random() * 3) + 0)
+		console.log(this.randomNumber);
 	}
 	
+	whoWon(userVal)
+	{
+		if(userVal === 0)
+		{
+			if(this.randomNumber === 0)
+			{
+				console.log("TIE Rock V Rock");
+			}
+			else if(this.randomNumber === 1)
+			{
+				console.log("BOT WINS Rock V PAPER");
+			}
+			else if(this.randomNumber === 2)
+			{
+				console.log("PLAYER WINS Rock V SCISSORS");
+			}
+		}
+		else if(userVal === 1)
+		{
+			if(this.randomNumber === 0)
+			{
+				console.log("PLAYER WINS PAPER V Rock");
+			}
+			else if(this.randomNumber === 1)
+			{
+				console.log("TIE PAPER V PAPER");
+			}
+			else if(this.randomNumber === 2)
+			{
+				console.log("BOT WINS PAPER V SCISSORS");
+			}
+		}
+		else if(userVal === 2)
+		{
+			if(this.randomNumber === 0)
+			{
+				console.log("BOT WINS SCISSORS V Rock");
+			}
+			else if(this.randomNumber === 1)
+			{
+				console.log("PLAYER WINS SCISSORS V PAPER");
+			}
+			else if(this.randomNumber === 2)
+			{
+				console.log("TIE SCISSORS V SCISSORS");
+			}
+		}
+	}
 }
 
-enterNameFrm.addEventListener("submit", function(ev){
+enterNameFrm.addEventListener("submit", function(ev)
+{
 	ev.preventDefault();
 	if(enterNameInpt.value === "")
 	{
 		return alert("Please enter a valid name.")
-		console.log("BAD")
 	}
 	else
 	{
 		name = enterNameInpt.value
-		console.log(name)
 		homeScreen.classList.add("hidden");
 		gameScreen.classList.remove("hidden");
 	}
+});
+
+enterChoiceFrm.addEventListener("submit", function(ev)
+{
+	ev.preventDefault();
+	console.log("TEST");
+	game = new RockPaperScissors();
+	var rockPaperOrScissors = parseInt(decision.options[decision.selectedIndex].value);
+	console.log(rockPaperOrScissors);
+	game.whoWon(rockPaperOrScissors);
 });
