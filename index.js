4234c833f184661bcb5423859d719521257ec793
kasubiaj <kasubiaj@mail.uc.edu>
2019-06-02 17:25:50 -0500

    Finished first form with logic to check if anything is entered. Started on second form

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 "use strict";
 const enterNameFrm = document.getElementById("enterNameFrm");
+const enterNameInpt = document.getElementById("enterNameInpt");
+
+var name;
 
 const homeScreen = document.getElementById("homeScreen");
 const gameScreen = document.getElementById("gameScreen");
 
+let game;
+
+class RockPaperScissors
+{
+	constructor()
+	{
+		
+	}
+	
+}
+
 enterNameFrm.addEventListener("submit", function(ev){
 	ev.preventDefault();
-	homeScreen.classList.add("hidden");
-	gameScreen.classList.remove("hidden");
+	if(enterNameInpt.value === "")
+	{
+		return alert("Please enter a valid name.")
+		console.log("BAD")
+	}
+	else
+	{
+		name = enterNameInpt.value
+		console.log(name)
+		homeScreen.classList.add("hidden");
+		gameScreen.classList.remove("hidden");
+	}
 });
